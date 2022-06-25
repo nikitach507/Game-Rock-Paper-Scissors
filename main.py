89a8d25cfba92795e624b4bd913cc132a58b8515
NikitaCh507 <chuprinnikita8@gmail.com>
2022-06-25 12:49:46 +0200

    Add second output

--- main.py
+++ main.py
@@ -4,29 +4,35 @@ game = True
 while game:
     print("k - kamen, n - nozhnici, b - bumaga, exit - vyhod")
     turn = input()
-    if turn == "k": print("kamen")
-    elif turn == "b": print("bumaga")
-    elif turn == "n": print("nozhnici")
+    if turn == "k": print("kamen - your turn")
+    elif turn == "b": print("bumaga - your turn")
+    elif turn == "n": print("nozhnici - your turn")
     elif turn == "exit":
         print("bye")
         game = False
     c_turn = random.choice(comp_turn)
+    if c_turn == "k":
+        c_turn = "kamen"
+    elif c_turn == "b":
+        c_turn = "bumaga"
+    elif c_turn == "n":
+        c_turn = "nozhnici"
     print(c_turn + " - computer turn")
-    if turn == "k" and c_turn == "n":
+    if turn == "k" and c_turn == "nozhnici":
         print("You win")
-    if turn == "n" and c_turn == "b":
+    if turn == "n" and c_turn == "bumaga":
             print("You win")
-    if turn == "b" and c_turn == "k":
+    if turn == "b" and c_turn == "kamen":
                 print("You win")
-    if turn == "k" and c_turn == "b":
+    if turn == "k" and c_turn == "bumaga":
         print("You lose")
-    if turn == "n" and c_turn == "k":
+    if turn == "n" and c_turn == "kamen":
             print("You lose")
-    if turn == "b" and c_turn == "n":
+    if turn == "b" and c_turn == "nozhnici":
                 print("You lose")
-    if turn == "b" and c_turn == "b":
+    if turn == "b" and c_turn == "bumaga":
         print("Draw")
-    if turn == "n" and c_turn == "n":
+    if turn == "n" and c_turn == "nozhnici":
             print("Draw")
-    if turn == "k" and c_turn == "k":
+    if turn == "k" and c_turn == "kamen":
                 print("Draw")
